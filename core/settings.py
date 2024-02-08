35d41b26da95ac5d1d6c31d7b384a7d126f92ced
BwanaQ <thunjawax@gmail.com>
2024-02-08 14:07:41 +0300

    add whitelisted urls

--- core/settings.py
+++ core/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-8i9@ghn-&=!mqb5+_d#prg9+9s@lpele^9h+x)d9r(q_#@huek
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost','fsdj.vercel.app' ]
 
 
 # Application definition
